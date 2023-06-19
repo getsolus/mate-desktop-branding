50101fc3837a619d38072efec2cf52c0de059b78
Joey Riches <josephriches@gmail.com>
2023-06-19 15:57:02 +0100

    Bump version to v20

--- mkrelease.sh
+++ mkrelease.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-VERSION="19"
+VERSION="20"
 NAME="mate-desktop-branding"
 git-archive-all --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
